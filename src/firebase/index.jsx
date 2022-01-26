7e108c95d374052a5848931122639fe591328222
Lucas Macias <chueko@gmail.com>
2022-01-26 15:15:04 -0300

    firebase2

--- src/firebase/index.jsx
+++ src/firebase/index.jsx
@@ -11,18 +11,13 @@ const app = firebase.initializeApp({
     messagingSenderId: "374471900554",
     appId: "1:374471900554:web:042b25934ca1ee2b055c73"
 
-    // apiKey: "AIzaSyDIeR8d5Qh93J45IfVgpahHV3JWLvpZPVU",
-    // authDomain: "spotba-e10b0.firebaseapp.com",
-    // projectId: "spotba-e10b0",
-    // storageBucket: "spotba-e10b0.appspot.com",
-    // messagingSenderId: "685829136618",
-    // appId: "1:685829136618:web:a8f4c3a3ee6e43cff57793"
+
 })
 
-  export const getFirebase=()=>{
-      return app
-  }
+export const getFirebase=()=>{
+    return app
+}
 
-  export const getFirestore=()=>{
-      return firebase.firestore(app)
-  }
+export const getFirestore=()=>{
+    return firebase.firestore(app)
+}
